945324c455effd4a1bcabafd2a3942d5f83ef258
Len Dierickx <lendierickx@LenDierckxsiMac.fritz.box>
2017-10-18 23:00:07 +0200

    Linting and omit todo

---
+++ src/object/omit.js
@@ -0,0 +1,17 @@
+/**
+ * @author Len Dierickx
+ * equals
+ * checks for equality
+ */
+import curry from './curry'
+import exists from './exists'
+import identical from './identical'
+import type from './type'
+import not from './not'
+import and from '../util/and'
+import isObject from './is'
+
+const omit = curry((names, obj) => {
+  // TODO 
+  return undefined
+}
