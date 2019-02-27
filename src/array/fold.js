87f83643112ff4111ce340d7614cae8fb5746650
sitefig <len.dierickx@gmail.com>
2019-02-27 17:40:43 +0100

    Cleaning up documentation

--- src/array/fold.js
+++ src/array/fold.js
@@ -1,7 +1,16 @@
 /**
+ * @function fold
+ * @description Returns a single item by mapping over the provided array and calling an iterator function
+ * @param {Function} cb - Callback function to modify the item
+ * @param {*} init - Initial value
+ * @param {Array} a - Array with items to modify by the cb function
+ * @alias reduce
+ * @since 1.0.2
  * @author Len Dierickx
- * fold
- *
+ * @example
+ * 
+ * const result = fold(add, 0, [1,1,1])
+ * // result = 3
  */
 import curry from '../util/curry'
 
--- src/array/is.js
+++ src/array/is.js
@@ -1,6 +1,16 @@
 /**
- * Created by dierickx.len on 01/04/2017.
+ * @function isArray
+ * @description Checks if the provided object is an array, autocurried function 
+ * @param {*} Object to verifiy
+ * @returns {boolean}
+ * @since 1.0.2
+ * @author Len Dierickx
+ * @example
+ * 
+ * const result = isArray([1,1,1])
+ * // result = true
  */
+
 import is from '../util/is'
 
 export default is('Array')
