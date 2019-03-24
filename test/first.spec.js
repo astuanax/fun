6105a476e5c9919913c2ca748bf04656e6394128
Sitefig <social@sitefig.eu>
2019-03-24 13:29:48 +0100

    Add first function, test and docs

---
+++ test/first.spec.js
@@ -0,0 +1,19 @@
+import chai from 'chai'
+import first from '../src/first'
+
+describe('first', function () {
+  it('returns first element of array', () => {
+    chai.expect(first([1, 2, 4])).to.equal(1)
+  })
+
+  it('returns first element of string', () => {
+    chai.expect(first('abcd')).to.equal('a')
+  })
+
+  it('returns undefined', () => {
+    chai.expect(first(undefined)).to.equal(undefined)
+    chai.expect(first([])).to.equal(undefined)
+    chai.expect(first({})).to.equal(undefined)
+    chai.expect(first(null)).to.equal(undefined)
+  })
+})
