9d97c16d3505f86aaaa718da817b2a94c0f5d9d0
Sitefig <social@sitefig.eu>
2019-04-21 13:26:50 +0200

    Add Maybe docs

--- src/monads/Maybe.js
+++ src/monads/Maybe.js
@@ -12,6 +12,7 @@ let Maybe = function (val) {
 }
 
 /**
+ * @memberOf Maybe
  * @function of
  * @desc Creates a Maybe monad from the provided argument
  * @param val
@@ -27,6 +28,7 @@ Maybe.of = function (val) {
 }
 
 /**
+ * @memberOf Maybe
  * @function getOrElse
  * @desc Evaluates the Maybe monad and returns either the value from the Maybe or the default value
  * @param n Default value to return if the Maybe evaluates to undefined or null
@@ -36,6 +38,12 @@ Maybe.prototype.getOrElse = function (n) {
   return this.isNothing() ? n : this.__value
 }
 
+/**
+ * @memberOf Maybe
+ * @function map
+ * @param f
+ * @returns {Maybe}
+ */
 Maybe.prototype.map = function (f) {
   if (this.isNothing()) {
     return Maybe.of(null)
@@ -43,26 +51,60 @@ Maybe.prototype.map = function (f) {
   return Maybe.of(f(this.__value))
 }
 
+/**
+ * @memberOf Maybe
+ * @function flatMap
+ * @param f
+ * @returns {Maybe}
+ */
 Maybe.prototype.flatMap = function (f) {
   return this.map(f)
 }
 
+/**
+ * @memberOf Maybe
+ * @function ap
+ * @param m
+ * @returns {Maybe}
+ */
 Maybe.prototype.ap = function (m) {
   return Maybe.of(m).map(this.__value)
 }
 
+/**
+ * @memberOf Maybe
+ * @function isNothing
+ * @returns {boolean}
+ */
 Maybe.prototype.isNothing = function () {
   return (this.__value == null)
 }
 
+/**
+ * @memberOf Maybe
+ * @function isJust
+ * @returns {boolean}
+ */
 Maybe.prototype.isJust = function () {
   return !this.isNothing()
 }
 
+/**
+ * @memberOf Maybe
+ * @function Nothing
+ * @returns {Nothing}
+ * @constructor
+ */
 Maybe.prototype.Nothing = function () {
   return new Nothing()
 }
 
+/**
+ * @memberOf Maybe
+ * @function Just
+ * @returns {Just}
+ * @constructor
+ */
 Maybe.prototype.Just = function () {
   return new Just(this._value)
 }
